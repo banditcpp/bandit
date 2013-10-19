8f41b079afeb644fb2a5dd9685847bcc40d37623
Joakim Karlsson <joakim@jkarlsson.com>
2013-10-19 13:48:52 +0200

    chore(build): removed files automatically generated by cotire

--- cotire/bandit-specs_CXX_prefix.hxx
+++
@@ -1,5 +0,0 @@
-/* cotire.cmake 1.4.1 generated file */
-/* /Users/jok/Documents/sw/github/bandit/cotire/bandit-specs_CXX_prefix.hxx */
-#ifdef __cplusplus
-#include "/Users/jok/Documents/sw/github/bandit/specs/specs.h"
-#endif
