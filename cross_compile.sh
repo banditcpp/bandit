13c86fc5f6ad94d26937803709ae4ba548ab92d1
Joakim Karlsson <joakim@jkarlsson.com>
2015-07-24 11:26:51 +0200

    Adds gcc5 and clang3.6 to cross_compile script

--- cross_compile.sh
+++ cross_compile.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+build_results=()
+
 function build_for {
   CC=$1
   CXX=$2
-  BUILD_DIR=build-$CC
+  BUILD_DIR=$CC
 
   mkdir $BUILD_DIR
   pushd $BUILD_DIR
   CC=$CC CXX=$CXX cmake ../..
   make
-  build_result=$?
+  build_results+=("$CC: $?")
   popd
 }
 
@@ -20,33 +22,22 @@ fi
 mkdir builds
 pushd builds
 
-build_for gcc-4.5 g++-4.5
-GCC45=$build_result
-
-build_for gcc-4.6 g++-4.6
-GCC46=$build_result
-
-build_for gcc-4.7 g++-4.7
-GCC47=$build_result
-
-build_for gcc-4.8 g++-4.8
-GCC48=$build_result
-
-build_for gcc-4.9 g++-4.9
-GCC49=$build_result
-
+build_for clang-3.6 clang++-3.6
+build_for gcc-5 g++-5
 build_for clang clang++
-CLANG=$build_result
+build_for gcc-4.9 g++-4.9
+build_for gcc-4.8 g++-4.8
+build_for gcc-4.7 g++-4.7
+build_for gcc-4.6 g++-4.6
+build_for gcc-4.5 g++-4.5
 
 popd
 
 echo
 echo "Result:"
-echo -e "gcc-4.5:\t$GCC45"
-echo -e "gcc-4.6:\t$GCC46"
-echo -e "gcc-4.7:\t$GCC47"
-echo -e "gcc-4.8:\t$GCC48"
-echo -e "gcc-4.9:\t$GCC49"
-echo -e "clang:\t$CLANG"
+for res in "${build_results[@]}"
+do
+    echo $res
+done
+
 echo "Done"
-exit $(( $GCC45 + $GCC46 + $GCC47 + $GCC48 + $GCC49 + $CLANG ))
