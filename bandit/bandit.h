9c554efb99e993a8f5e987e8b5dc4fcd25e77310
Joakim Karlsson <joakim@jkarlsson.com>
2015-07-30 09:31:52 +0200

    Ensuring that MSVC specific stuff doesn't affect clang/gcc
    The checks for what version of MSVC we're using needs to check that
    we're actually using MSVC first. Otherwise this will undefine `noexcept`
    for non-MS compilers.

--- bandit/bandit.h
+++ bandit/bandit.h
@@ -1,10 +1,12 @@
 #ifndef BANDIT_BANDIT_H
 #define BANDIT_BANDIT_H
 
+#ifdef _MSC_VER
 #if (_MSC_VER < 1900)
 // Visual Studio versions before 2015 do not support the noexcept keyword
 #define _ALLOW_KEYWORD_MACROS
 #define noexcept
+#endif
 #endif
 
 #include <cassert>
