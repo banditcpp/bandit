0895bdd8299447f9c8bcf40a7f9ce8907aec3825
nabijaczleweli <nabijaczleweli@gmail.com>
2016-01-11 15:49:53 +0100

    Replace all occurences of 'time="0"' with 'time="0.0"' for XUnit reporter
    Closes https://github.com/joakimkarlsson/bandit/issues/70

--- bandit/reporters/xunit_reporter.h
+++ bandit/reporters/xunit_reporter.h
@@ -19,7 +19,7 @@ namespace bandit { namespace detail {
     {
       progress_reporter::it_starting(desc);
       work_stm_ << "\t<testcase classname=\"" << escape(current_context_name()) << "\" ";
-      work_stm_ << "name=\"" << escape(desc) << "\" time=\"0\">\n";
+      work_stm_ << "name=\"" << escape(desc) << "\" time=\"0.0\">\n";
     }
 
     void it_succeeded(const char* desc)
@@ -46,7 +46,7 @@ namespace bandit { namespace detail {
     {
       progress_reporter::it_skip(desc);
       work_stm_ << "\t<testcase classname=\"" << escape(current_context_name()) << "\" ";
-      work_stm_ << "name=\"" << escape(desc) << "\" time=\"0\">\n";
+      work_stm_ << "name=\"" << escape(desc) << "\" time=\"0.0\">\n";
       work_stm_ << "\t\t<skipped />\n";
       work_stm_ << "\t</testcase>\n";
     }
@@ -54,14 +54,14 @@ namespace bandit { namespace detail {
     void test_run_complete()
     {
       stm_ << "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n";
-      stm_ << "<testsuite name=\"bandit\" tests=\"" << specs_run_ << "\" errors=\"0\" failures=\"" 
+      stm_ << "<testsuite name=\"bandit\" tests=\"" << specs_run_ << "\" errors=\"0\" failures=\""
            << specs_failed_ << "\"";
-      
+
       if(specs_skipped_ > 0)
       {
         stm_ << " skipped=\"" << specs_skipped_ << "\"";
       }
-      
+
       stm_ << ">\n";
 
       stm_ << work_stm_.str();
