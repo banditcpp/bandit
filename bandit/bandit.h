eea31ed672c211ce6a33fc6c7cc8fa79b30e06e6
Stephan Beyer <s-beyer@gmx.net>
2016-10-03 19:15:55 +0200

    Minimize bandit.h to main bandit: grammar and runner

--- bandit/bandit.h
+++ bandit/bandit.h
@@ -9,28 +9,6 @@
 #endif
 #endif
 
-#include <algorithm>
-#include <cassert>
-#include <iostream>
-#include <list>
-#include <memory>
-#include <deque>
-#include <sstream>
-#include <stdexcept>
-#include <vector>
-
-#include <bandit/version.h>
-
-#include <bandit/types.h>
-#include <bandit/test_run_error.h>
-#include <bandit/registration/registrar.h>
-#include <bandit/assertion_exception.h>
-#include <bandit/failure_formatters/failure_formatters.h>
-#include <bandit/adapters/adapters.h>
-#include <bandit/listener.h>
-#include <bandit/reporters/reporters.h>
-#include <bandit/context.h>
-#include <bandit/run_policies/run_policies.h>
 #include <bandit/grammar.h>
 #include <bandit/runner.h>
 
