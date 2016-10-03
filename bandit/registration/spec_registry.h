577d7b46947f533b4a96cb717da6624d23260d0a
Stephan Beyer <s-beyer@gmx.net>
2016-10-03 17:55:38 +0200

    Make spec_registry.h self-sufficient

--- bandit/registration/spec_registry.h
+++ bandit/registration/spec_registry.h
@@ -1,6 +1,9 @@
 #ifndef BANDIT_SPEC_REGISTRY_H
 #define BANDIT_SPEC_REGISTRY_H
 
+#include <list>
+#include <bandit/types.h>
+
 namespace bandit {
   namespace detail {
     typedef std::list<voidfunc_t> spec_registry;
