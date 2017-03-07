2746ca33e87a875f30bfc414d6cde24c96ea4eb3
Stephan Beyer <s-beyer@gmx.net>
2017-03-07 21:29:36 +0100

    Make option handling a little smarter
    This introduces option handlers "Required", "Reporter"
    and "Formatter".
    
    Also --break-on-failure and --dry-run do not have an optional
    argument, here the "None" option handler must be used.
    This is currently without effect since ARG_ILLEGAL does not
    lead to an error.

--- bandit/options.h
+++ bandit/options.h
@@ -1,6 +1,7 @@
 #ifndef BANDIT_OPTIONS_H
 #define BANDIT_OPTIONS_H
 
+#include <algorithm>
 #include <vector>
 #include <iostream>
 
@@ -12,6 +13,63 @@ namespace bandit { namespace detail {
     // TODO: check for parser errors
     struct options
     {
+      struct argument : public option::Arg {
+        static const std::vector<std::string> reporter_list()
+        {
+          return {
+            "dots",
+            "singleline",
+            "xunit",
+            "info",
+            "spec",
+          };
+        }
+
+        static const std::vector<std::string> formatter_list()
+        {
+          return {
+            "default",
+            "vs",
+          };
+        }
+
+        static option::ArgStatus Required(const option::Option& option, bool msg)
+        {
+          if (option.arg != nullptr) {
+            return option::ARG_OK;
+          }
+          if (msg) {
+            std::cerr << "Option '" << option.name << "' requires an argument\n";
+          }
+          return option::ARG_ILLEGAL;
+        }
+
+        static option::ArgStatus OneOf(const option::Option& option, bool msg, const std::vector<std::string> &&list)
+        {
+          auto status = Required(option, msg);
+          if (status == option::ARG_OK
+           && std::find(list.begin(), list.end(), option.arg) == list.end()) {
+            if (msg) {
+              std::cerr << "Option argument of '" << option.name << "' must be one of:\n";
+              for (auto item : list) {
+                std::cerr << " * " << item << std::endl;
+              }
+            }
+            status = option::ARG_ILLEGAL;
+          }
+          return status;
+        }
+
+        static option::ArgStatus Reporter(const option::Option& option, bool msg)
+        {
+          return OneOf(option, msg, reporter_list());
+        }
+
+        static option::ArgStatus Formatter(const option::Option& option, bool msg)
+        {
+          return OneOf(option, msg, formatter_list());
+        }
+      };
 
       options(int argc, char* argv[])
       {
@@ -86,34 +144,54 @@ namespace bandit { namespace detail {
           return options_[DRY_RUN] != NULL;
       }
 
-      private:
-        enum option_index { UNKNOWN, VERSION, HELP, REPORTER, NO_COLOR,
-          FORMATTER, SKIP, ONLY, BREAK_ON_FAILURE, DRY_RUN };
+    private:
+      enum option_index {
+        UNKNOWN,
+        VERSION,
+        HELP,
+        REPORTER,
+        NO_COLOR,
+        FORMATTER,
+        SKIP,
+        ONLY,
+        BREAK_ON_FAILURE,
+        DRY_RUN,
+      };
 
-        static const option::Descriptor* usage()
-        {
-          static const option::Descriptor usage[] =
-          {
-            {UNKNOWN, 0, "", "", option::Arg::None,         "USAGE: <executable> [options]\n\n"
-                                                            "Options:" },
-            {VERSION, 0, "", "version", option::Arg::None, "  --version, \tPrint version of bandit"},
-            {HELP, 0, "", "help", option::Arg::None,        "  --help, \tPrint usage and exit."},
-            {REPORTER, 0, "", "reporter", option::Arg::Optional, "  --reporter=<reporter>, \tSelect reporter (dots, singleline, xunit, info, spec)"},
-            {NO_COLOR, 0, "", "no-color", option::Arg::None,     "  --no-color, \tSuppress colors in output"},
-            {FORMATTER, 0, "", "formatter", option::Arg::Optional, "  --formatter=<formatter>, \tSelect formatting of errors (default, vs)"},
-            {SKIP, 0, "", "skip", option::Arg::Optional, "  --skip=<substring>, \tskip all 'describe' and 'it' containing substring"},
-            {ONLY, 0, "", "only", option::Arg::Optional, "  --only=<substring>, \tonly run 'describe' and 'it' containing substring"},
-            {BREAK_ON_FAILURE, 0, "", "break-on-failure", option::Arg::Optional, "  --break-on-failure, \tstop test run on first failing test"},
-            {DRY_RUN, 0, "", "dry-run", option::Arg::Optional, "  --dry-run, \tdon't run tests. Just list progress. Use to list available tests"},
-            {0, 0, 0, 0, 0, 0}
-          };
+      static const option::Descriptor* usage()
+      {
+        static const option::Descriptor usage[] = {
+          { UNKNOWN, 0, "", "", argument::None,
+            "USAGE: <executable> [options]\n\n"
+            "Options:" },
+          { VERSION, 0, "", "version", argument::None,
+            "  --version, \tPrint version of bandit" },
+          { HELP, 0, "", "help", argument::None,
+            "  --help, \tPrint usage and exit." },
+          { REPORTER, 0, "", "reporter", argument::Reporter,
+            "  --reporter=<reporter>, \tSelect reporter" },
+          { NO_COLOR, 0, "", "no-color", argument::None,
+            "  --no-color, \tSuppress colors in output" },
+          { FORMATTER, 0, "", "formatter", argument::Formatter,
+            "  --formatter=<formatter>, \tSelect formatting of errors" },
+          { SKIP, 0, "", "skip", argument::Required,
+            "  --skip=<substring>, \tskip all 'describe' and 'it' containing substring" },
+          { ONLY, 0, "", "only", argument::Required,
+            "  --only=<substring>, \tonly run 'describe' and 'it' containing substring" },
+          { BREAK_ON_FAILURE, 0, "", "break-on-failure", argument::None,
+            "  --break-on-failure, \tstop test run on first failing test" },
+          { DRY_RUN, 0, "", "dry-run", argument::None,
+            "  --dry-run, \tdon't run tests, just list progress."
+            "Use to list available tests" },
+          { 0, 0, 0, 0, 0, 0 }
+        };
 
-          return usage;
-        }
+        return usage;
+      }
 
-      private:
-        std::vector<option::Option> options_;
-        bool parsed_ok_;
+    private:
+      std::vector<option::Option> options_;
+      bool parsed_ok_;
 
     };
 
