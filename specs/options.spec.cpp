4fc6942cc448a1ed09517bf582d4ea80b2a4d5a5
Stephan Beyer <s-beyer@gmx.net>
2017-03-17 03:28:42 +0100

    Make options spec more concise
    The auxiliary class "options" unifies argv_helper and
    the bandit's original options class to allow more concise tests.

--- specs/options.spec.cpp
+++ specs/options.spec.cpp
@@ -10,106 +10,85 @@ static void all_ok(const bd::options &opt) {
   AssertThat(opt.has_unknown_options(), IsFalse());
 }
 
+struct options : private argv_helper, public bd::options {
+  options(std::list<std::string>&& args)
+    : argv_helper(std::move(args))
+    , bd::options(argc(), argv())
+  {}
+};
+
 go_bandit([](){
 
     describe("options:", [&](){
 
       it("parses the '--help' option", [&](){
-        argv_helper argv({"--help"});
-
-        bd::options opt(argv.argc(), argv.argv());
-
+        options opt({"--help"});
         AssertThat(opt.help(), IsTrue());
         all_ok(opt);
       });
 
       it("parses the '--version' option", [&](){
-        argv_helper argv({"--version"});
-
-        bd::options opt(argv.argc(), argv.argv());
-
+        options opt({"--version"});
         AssertThat(opt.version(), IsTrue());
         all_ok(opt);
       });
 
       it("parses the '--no-color' option", [&](){
-        argv_helper argv({"--no-color"});
-
-        bd::options opt(argv.argc(), argv.argv());
-
+        options opt({"--no-color"});
         AssertThat(opt.no_color(), IsTrue());
         all_ok(opt);
       });
 
       it("parses the '--formatter=vs' option", [&](){
-        argv_helper argv({"--formatter=vs"});
-
-        bd::options opt(argv.argc(), argv.argv());
+        options opt({"--formatter=vs"});
         AssertThat(opt.formatter(), Equals(bd::options::formatters::FORMATTER_VS));
         all_ok(opt);
       });
 
       it("parses the '--formatter=default' option", [&](){
-        argv_helper argv({"--formatter=default"});
-
-        bd::options opt(argv.argc(), argv.argv());
+        options opt({"--formatter=default"});
         AssertThat(opt.formatter(), Equals(bd::options::formatters::FORMATTER_DEFAULT));
         all_ok(opt);
       });
 
       it("parses the '--skip=\"substring\"' option", [&](){
-        argv_helper argv({"--skip=substring"});
-
-        bd::options opt(argv.argc(), argv.argv());
+        options opt({"--skip=substring"});
         AssertThat(opt.skip(), Equals("substring"));
         all_ok(opt);
       });
 
       it("parses skip as empty string if not present", [&](){
-        argv_helper argv({});
-
-        bd::options opt(argv.argc(), argv.argv());
+        options opt({});
         AssertThat(opt.skip(), Equals(""));
         all_ok(opt);
       });
 
       it("parses the '--only=\"substring\"' option", [&](){
-        argv_helper argv({"--only=substring"});
-
-        bd::options opt(argv.argc(), argv.argv());
+        options opt({"--only=substring"});
         AssertThat(opt.only(), Equals("substring"));
         all_ok(opt);
       });
 
       it("parses only as empty string if not present", [&](){
-        argv_helper argv({});
-
-        bd::options opt(argv.argc(), argv.argv());
+        options opt({});
         AssertThat(opt.only(), Equals(""));
         all_ok(opt);
       });
 
       it("parses the '--break-on-failure' option", [&](){
-        argv_helper argv({"--break-on-failure"});
-
-        bd::options opt(argv.argc(), argv.argv());
-
+        options opt({"--break-on-failure"});
         AssertThat(opt.break_on_failure(), IsTrue());
         all_ok(opt);
       });
 
       it("parses the '--dry-run' option", [&](){
-        argv_helper argv({"--dry-run"});
-
-        bd::options opt(argv.argc(), argv.argv());
-
+        options opt({"--dry-run"});
         AssertThat(opt.dry_run(), IsTrue());
         all_ok(opt);
       });
 
       describe("with no arguments", [&](){
-        argv_helper argv({});
-        bd::options opt(argv.argc(), argv.argv());
+        options opt({});
 
         it("is valid", [&] {
           all_ok(opt);
