d1de7d042c994422b0138e9406791f5308c176c1
Stephan Beyer <s-beyer@gmx.net>
2017-08-14 22:03:29 +0200

    Improve readability of colorizer spec

--- specs/colorizer.cpp
+++ specs/colorizer.cpp
@@ -4,30 +4,31 @@ go_bandit([]() {
   describe("colorizers", [&]() {
 #if !defined(_WIN32) || defined(BANDIT_CONFIG_COLOR_ANSI)
     describe("colorizer::light", [&]() {
+      colorizer::backend color;
       colorizer::light colorizer;
 
       it("sets 'bad' color to red", [&]() {
-        AssertThat(colorizer.bad(), Equals("\033[1;31m"));
+        AssertThat(colorizer.bad(), Equals(color.red()));
       });
 
       it("sets 'good' color to green", [&]() {
-        AssertThat(colorizer.good(), Equals("\033[1;32m"));
+        AssertThat(colorizer.good(), Equals(color.green()));
       });
 
       it("sets 'neutral' color to yellow", [&]() {
-        AssertThat(colorizer.neutral(), Equals("\033[1;33m"));
+        AssertThat(colorizer.neutral(), Equals(color.yellow()));
       });
 
       it("sets 'info' color to blue", [&]() {
-        AssertThat(colorizer.info(), Equals("\033[1;34m"));
+        AssertThat(colorizer.info(), Equals(color.blue()));
       });
 
       it("emphasizes using color white", [&]() {
-        AssertThat(colorizer.emphasize(), Equals("\033[1;37m"));
+        AssertThat(colorizer.emphasize(), Equals(color.white()));
       });
 
       it("resets color", [&]() {
-        AssertThat(colorizer.reset(), Equals("\033[0m"));
+        AssertThat(colorizer.reset(), Equals(color.reset()));
       });
     });
 
@@ -65,27 +66,27 @@ go_bandit([]() {
       colorizer::off colorizer;
 
       it("ignores setting 'good' color", [&]() {
-        AssertThat(colorizer.good(), Equals(""));
+        AssertThat(colorizer.good(), IsEmpty());
       });
 
       it("ignores setting 'bad' color", [&]() {
-        AssertThat(colorizer.bad(), Equals(""));
+        AssertThat(colorizer.bad(), IsEmpty());
       });
 
       it("ignores setting 'neutral' color", [&]() {
-        AssertThat(colorizer.good(), Equals(""));
+        AssertThat(colorizer.good(), IsEmpty());
       });
 
       it("ignores setting 'info' color", [&]() {
-        AssertThat(colorizer.good(), Equals(""));
+        AssertThat(colorizer.good(), IsEmpty());
       });
 
       it("ignores emphasizing", [&]() {
-        AssertThat(colorizer.good(), Equals(""));
+        AssertThat(colorizer.good(), IsEmpty());
       });
 
       it("ignores resetting colors", [&]() {
-        AssertThat(colorizer.reset(), Equals(""));
+        AssertThat(colorizer.reset(), IsEmpty());
       });
     });
   });
