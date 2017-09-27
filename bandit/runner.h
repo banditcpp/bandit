d783d083bbfe3f8e1cbbcbe21afe1c86f0ddc1c0
Stephan Beyer <s-beyer@gmx.net>
2017-09-27 20:44:28 +0200

    Introduce bd::settings struct containing chosen classes
    ...like the formatter, the colorizer, the reporter and the run policy.

--- bandit/runner.h
+++ bandit/runner.h
@@ -10,53 +10,63 @@
 
 namespace bandit {
   namespace detail {
-    inline run_policy_ptr create_run_policy(const options& opt) {
-      return run_policy_ptr(new run_policy::bandit(opt.filter_chain(), opt.break_on_failure(), opt.dry_run()));
-    }
+    struct settings {
+      colorizer_ptr colorizer;
+      failure_formatter_ptr formatter;
+      reporter_ptr reporter;
+      run_policy_ptr run_policy;
 
-    inline reporter_ptr create_reporter(const options& opt,
-        const failure_formatter_t& formatter, const colorizer_t& colorizer) {
-      switch (opt.reporter()) {
-      case options::reporters::SINGLELINE:
-        return reporter_ptr(new bandit::reporter::singleline(formatter, colorizer));
-      case options::reporters::XUNIT:
-        return reporter_ptr(new bandit::reporter::xunit(formatter));
-      case options::reporters::INFO:
-        return reporter_ptr(new bandit::reporter::info(formatter, colorizer));
-      case options::reporters::SPEC:
-        return reporter_ptr(new bandit::reporter::spec(formatter, colorizer));
-      case options::reporters::CRASH:
-        return reporter_ptr(new bandit::reporter::crash(formatter));
-      case options::reporters::DOTS:
-      default:
-        return reporter_ptr(new bandit::reporter::dots(formatter, colorizer));
+      settings(const options& opt)
+          : colorizer(create_colorizer(opt)),
+            formatter(create_formatter(opt)),
+            reporter(create_reporter(opt, *formatter, *colorizer)),
+            run_policy(new run_policy::bandit(opt.filter_chain(), opt.break_on_failure(), opt.dry_run())) {}
+
+    private:
+      static inline colorizer_ptr create_colorizer(const options& opt) {
+        switch (opt.colorizer()) {
+        case options::colorizers::OFF:
+          return colorizer_ptr(new colorizer::off());
+        case options::colorizers::DARK:
+          return colorizer_ptr(new colorizer::dark());
+        case options::colorizers::LIGHT:
+        default:
+          return colorizer_ptr(new colorizer::light());
+        }
       }
-    }
+
+      static inline failure_formatter_ptr create_formatter(const options& opt) {
+        switch (opt.formatter()) {
+        case options::formatters::VS:
+          return failure_formatter_ptr(new failure_formatter::visual_studio());
+        case options::formatters::POSIX:
+        default:
+          return failure_formatter_ptr(new failure_formatter::posix());
+        }
+      }
+
+      static inline reporter_ptr create_reporter(const options& opt,
+          const failure_formatter_t& formatter, const colorizer_t& colorizer) {
+        switch (opt.reporter()) {
+        case options::reporters::SINGLELINE:
+          return reporter_ptr(new bandit::reporter::singleline(formatter, colorizer));
+        case options::reporters::XUNIT:
+          return reporter_ptr(new bandit::reporter::xunit(formatter));
+        case options::reporters::INFO:
+          return reporter_ptr(new bandit::reporter::info(formatter, colorizer));
+        case options::reporters::SPEC:
+          return reporter_ptr(new bandit::reporter::spec(formatter, colorizer));
+        case options::reporters::CRASH:
+          return reporter_ptr(new bandit::reporter::crash(formatter));
+        case options::reporters::DOTS:
+        default:
+          return reporter_ptr(new bandit::reporter::dots(formatter, colorizer));
+        }
+      }
+    };
 
     using reporter_factory_fn = std::function<reporter_ptr(const std::string&, const failure_formatter_t*)>;
     using register_reporter_fn = std::function<detail::reporter_t*(detail::reporter_t*)>;
-
-    inline failure_formatter_ptr create_formatter(const options& opt) {
-      switch (opt.formatter()) {
-      case options::formatters::VS:
-        return failure_formatter_ptr(new failure_formatter::visual_studio());
-      case options::formatters::POSIX:
-      default:
-        return failure_formatter_ptr(new failure_formatter::posix());
-      }
-    }
-
-    inline colorizer_ptr create_colorizer(const options& opt) {
-      switch (opt.colorizer()) {
-      case options::colorizers::OFF:
-        return colorizer_ptr(new colorizer::off());
-      case options::colorizers::DARK:
-        return colorizer_ptr(new colorizer::dark());
-      case options::colorizers::LIGHT:
-      default:
-        return colorizer_ptr(new colorizer::light());
-      }
-    }
   }
 
   inline int run(const detail::options& opt, const detail::spec_registry& specs,
@@ -94,15 +104,11 @@ namespace bandit {
       return 1;
     }
 
-    detail::failure_formatter_ptr formatter(create_formatter(opt));
-    detail::colorizer_ptr colorizer(create_colorizer(opt));
-    detail::reporter_ptr reporter(create_reporter(opt, *formatter, *colorizer));
-    detail::run_policy_ptr run_policy(create_run_policy(opt));
+    detail::settings settings(opt);
+    detail::register_reporter(settings.reporter.get());
+    detail::register_run_policy(settings.run_policy.get());
 
-    detail::register_reporter(reporter.get());
-    detail::register_run_policy(run_policy.get());
-
-    return run(opt, detail::specs(), context::stack(), *reporter);
+    return run(opt, detail::specs(), context::stack(), *settings.reporter);
   }
 }
 #endif
