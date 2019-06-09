8887b795aab1131f82751ede25ecbd3aa54e5c81
Martin Delille <martin@delille.org>
2019-06-09 18:59:56 +0200

    fix qt_creator failure_formatters spec

--- specs/failure_formatters/qt_creator.cpp
+++ specs/failure_formatters/qt_creator.cpp
@@ -7,7 +7,7 @@ go_bandit([]() {
 
     it("formats assertions with file and line number", [&]() {
       bd::assertion_exception exception("message", "file", 321);
-      AssertThat(formatter.format(exception), Equals("file:321: message"));
+      AssertThat(formatter.format(exception), Equals("file://file:321: message"));
     });
 
     it("formats assertions without file and line number", [&]() {
