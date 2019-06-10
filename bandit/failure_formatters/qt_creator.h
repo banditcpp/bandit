b0c055685e27c603fc57a51776e444214c438931
Stephan Beyer <s-beyer@gmx.net>
2019-06-10 21:33:31 +0200

    Get rid of code duplication in failure formatters
    We keep the code duplication for the failure formatter specs,
    for simplicity (although it would be a nice show-case about
    the flexibility of bandit if we also replace the code duplication
    in the specs).

--- bandit/failure_formatters/qt_creator.h
+++ bandit/failure_formatters/qt_creator.h
@@ -1,27 +1,13 @@
 #ifndef BANDIT_FAILURE_FORMATTERS_QT_CREATOR_H
 #define BANDIT_FAILURE_FORMATTERS_QT_CREATOR_H
 
-#include <sstream>
-#include <bandit/failure_formatters/interface.h>
+#include <bandit/failure_formatters/generic.h>
 
 namespace bandit {
   namespace failure_formatter {
-    struct qt_creator : public interface {
+    struct qt_creator : public generic {
       std::string format(const detail::assertion_exception& err) const override {
-        std::stringstream ss;
-        if (err.file_name().size()) {
-          ss << "file://" << err.file_name();
-
-          if (err.line_number()) {
-            ss << ":" << err.line_number();
-          }
-
-          ss << ": ";
-        }
-
-        ss << err.what();
-
-        return ss.str();
+        return generic_format(err, "file://", ":", "", ": ", "");
       }
     };
   }
--- bandit/failure_formatters/visual_studio.h
+++ bandit/failure_formatters/visual_studio.h
@@ -1,29 +1,13 @@
 #ifndef BANDIT_FAILURE_FORMATTERS_VISUAL_STUDIO_H
 #define BANDIT_FAILURE_FORMATTERS_VISUAL_STUDIO_H
 
-#include <sstream>
-#include <bandit/failure_formatters/interface.h>
+#include <bandit/failure_formatters/generic.h>
 
 namespace bandit {
   namespace failure_formatter {
-    struct visual_studio : public interface {
+    struct visual_studio : public generic {
       std::string format(const detail::assertion_exception& err) const override {
-        std::stringstream ss;
-        if (err.file_name().size()) {
-          ss << err.file_name();
-
-          if (err.line_number()) {
-            ss << "(" << err.line_number() << ")";
-          }
-
-          ss << ": ";
-        } else {
-          ss << "bandit: ";
-        }
-
-        ss << err.what();
-
-        return ss.str();
+        return generic_format(err, "", "(", ")", ": ", "bandit: ");
       }
     };
   }
